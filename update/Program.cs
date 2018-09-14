ba776a5de12cbb8653ef1e102b71679881bd592e
miounet <miounet@qq.com>
2018-09-14 14:06:59 +0800

    Add files via upload

---
+++ update/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
+using ICSharpCode.SharpZipLib;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Checksums;
+namespace update
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            string filepath = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\\"));
+            string file = System.IO.Path.Combine(filepath, "updatesoft.zip");
+            
+            int tn = 0;
+            //解压
+            if (File.Exists(file))
+            {
+            tryLb:
+                tn++;
+                System.Threading.Thread.Sleep(500);
+                if (UnZip(file, filepath, string.Empty))
+                {
+                    System.Threading.Thread.Sleep(300);
+                    File.Delete(file);//解压完成，删除下载的文件
+
+                    //关闭速录软件，打开升级解压程序
+
+                    System.Diagnostics.Process.Start(System.IO.Path.Combine(filepath, "SharpOut.exe"));
+
+                    Application.Exit();
+                }
+                else if (tn <= 3)
+                {
+                    System.Threading.Thread.Sleep(2500);
+                    goto tryLb;
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// 解压功能(解压压缩文件到指定目录)
+        /// </summary>
+        /// <param name="FileToUpZip">待解压的文件</param>
+        /// <param name="ZipedFolder">指定解压目标目录</param>
+        public static bool UnZip(string FileToUpZip, string ZipedFolder, string Password)
+        {
+            if (!File.Exists(FileToUpZip))
+            {
+                return true;
+            }
+
+            if (!Directory.Exists(ZipedFolder))
+            {
+                Directory.CreateDirectory(ZipedFolder);
+            }
+
+            ZipInputStream s = null;
+            ZipEntry theEntry = null;
+
+            string fileName;
+            FileStream streamWriter = null;
+            try
+            {
+                s = new ZipInputStream(File.OpenRead(FileToUpZip));
+                s.Password = Password;
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    if (theEntry.Name != String.Empty)
+                    {
+                        fileName = Path.Combine(ZipedFolder, theEntry.Name);
+                        ///判断文件路径是否是文件夹
+                        if (fileName.EndsWith("/") || fileName.EndsWith("//"))
+                        {
+                            Directory.CreateDirectory(fileName);
+                            continue;
+                        }
+
+                        streamWriter = File.Create(fileName);
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (streamWriter != null)
+                {
+                    streamWriter.Close();
+                    streamWriter = null;
+                }
+                if (theEntry != null)
+                {
+                    theEntry = null;
+                }
+                if (s != null)
+                {
+                    s.Close();
+                    s = null;
+                }
+                GC.Collect();
+                GC.Collect(1);
+            }
+        }
+    }
+}
