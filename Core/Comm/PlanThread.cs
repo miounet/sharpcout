112daeb4d4adc823083f3e1c0f49674b8c375aee
miounet <miounet@qq.com>
2022-02-23 18:27:26 +0800

    Update PlanThread.cs

--- Core/Comm/PlanThread.cs
+++ Core/Comm/PlanThread.cs
@@ -1,4 +1,4 @@
-﻿using Core.Base;
+using Core.Base;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,17 +23,19 @@ namespace Core.Comm
             running = true;
 #if !DEBUG
  
+          
+
+            //updateCloudDictTh = new Thread(new ThreadStart(UpdateCloudDict));
+            //updateCloudDictTh.IsBackground = true;
+            //updateCloudDictTh.Start();
+#endif
+            //getMyInfo = new Thread(new ThreadStart(GetMyInfo));
+            //getMyInfo.IsBackground = true;
+            //getMyInfo.Start();
+
             updateTh = new Thread(new ThreadStart(UpdateDict));
             updateTh.IsBackground = true;
             updateTh.Start();
-
-            updateCloudDictTh = new Thread(new ThreadStart(UpdateCloudDict));
-            updateCloudDictTh.IsBackground = true;
-            updateCloudDictTh.Start();
-#endif
-            getMyInfo = new Thread(new ThreadStart(GetMyInfo));
-            getMyInfo.IsBackground = true;
-            getMyInfo.Start();
         }
 
         public static void Stop()
@@ -82,99 +84,45 @@ namespace Core.Comm
                 }
                 UpdataSoft();
 
-             
-                Thread.Sleep(1000 * 60 * 60*12);
+                UpdataDict();
+                Thread.Sleep(1000 * 60 * 60 );
             }
         }
 
-        public static void UpdataSoft(bool flag=false)
+        public static void UpdataSoft(bool flag = false)
         {
             try
             {
-                
-                qzxxiEntity.UpdateDictEnt data = ApiClient.GetDictUpdate(Win.WinInput.ApiUrl);
-                if (data.UpdateFalg == 2)
-                {
-                    //现在就可升级词库
-                    #region 实现升级
-                    bool updataok = false;
-                    foreach (var du in data.UpdateUrl)
-                    {
-                        ///下载新词库
-                        if (MessageBox.Show("有新词库,是否下载!\r\n", "巧指速录-有新词库", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                        {
-                            Win.Login info = new Win.Login(false, Core.Win.WinInput.Input, 0);
-                            info.TopMost = true;
-                            info.Show();
-                            ApiClient.StartDictUpdate(Win.WinInput.ApiUrl);//开始download
-                            try
-                            {
-
-                                WebClient client = new WebClient();
-                                string filename = System.IO.Path.Combine(Win.WinInput.Input.AppPath, DateTime.Now.Ticks.ToString() + ".zip");
-                                client.DownloadFile(du, filename);
-                                Thread.Sleep(200);
-
-                                if (File.Exists(filename))
-                                {
-                                    //下载完成解压
-                                    updataok = ZipClass.UnZip(filename, System.IO.Path.Combine(Win.WinInput.Input.AppPath, "dict"), string.Empty);
-                                    Thread.Sleep(500);
-                                    File.Delete(filename);//解压完成，删除下载的文件
-                                    updataok = true;
-                                }
-                            }
-                            catch
-                            {
-                                updataok = false;
-                            }
-                            finally
-                            {
-                                ApiClient.EndDictUpdate(Win.WinInput.ApiUrl);//download完成
-                            }
-                        }
-                    }
-                    if (updataok)
-                    {
-                        Win.WinInput.DictVersion = data.DictVersion;
-                        Program.MIme.SaveSetting();
-                    }
-                    #endregion
-                    return;
-                }
-                else if (data.UpdateFalg == 3)
+                UpdateSoftEnt data = ApiClient.GetEnt(Win.WinInput.ApiUrl);
+                if (Program.ProductVer != data.ProductVer)
                 {
                     //下载最新版本
                     #region 实现升级
                     bool updataok = false;
-                    foreach (var du in data.UpdateUrl)
+                    string du = data.DownUrl;
+                    ///下载
+                    if (MessageBox.Show("有新版本,是否现在升级!\r\n", "速录宝-有新版本", MessageBoxButtons.YesNo) == DialogResult.Yes)
                     {
-                        ///下载
-                        if (MessageBox.Show("有新版本,是否现在升级!\r\n", "巧指速录-有新版本", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                        {
-                            Win.Login info = new Win.Login(false, Core.Win.WinInput.Input, 0);
-                            info.TopMost = true;
-                            info.Show();
-                            ApiClient.StartDictUpdate(Win.WinInput.ApiUrl);//开始download
-                            try
-                            {
+                        Win.Login info = new Win.Login(false, Core.Win.WinInput.Input, 0);
+                        info.TopMost = true;
+                        info.Show();
 
-                                WebClient client = new WebClient();
-                                string filename = System.IO.Path.Combine(Win.WinInput.Input.AppPath, "updatesoft.zip");
-                                client.DownloadFile(du, filename);
-                                Thread.Sleep(200);
-                                updataok = true;
-                            }
-                            catch
-                            {
-                                updataok = false;
-                            }
-                            finally
-                            {
-                                ApiClient.EndDictUpdate(Win.WinInput.ApiUrl);//download完成
-                            }
+                        try
+                        {
+                            WebClient client = new WebClient();
+                            client = new WebClient();
+                            string filename = System.IO.Path.Combine(Win.WinInput.Input.AppPath, "updatesoft.zip");
+                            client.DownloadFile(du, filename);
+                            Thread.Sleep(200);
+                            updataok = true;
                         }
+                        catch
+                        {
+                            updataok = false;
+                        }
+
                     }
+
                     if (updataok)
                     {
                         //关闭速录软件，打开升级解压程序
@@ -194,61 +142,119 @@ namespace Core.Comm
             }
             catch (Exception ex) { string ss = ex.ToString(); }
         }
-        private static void GetMyInfo()
+
+        public static void UpdataDict(bool flag = false)
         {
-            while (running)
+            try
             {
-                Thread.Sleep(40000);
-                try
+
+                UpdateEnt data = ApiClient.GetUpdateInfo(Win.WinInput.ApiDictUrl);
+                if (Win.WinInput.DictVersion != data.DictVersion)
                 {
-                    if (!Win.WinInput.Input.indexComplete)
+                    //现在就可升级词库
+                    #region 实现升级
+                    bool updataok = false;
+                    string du = data.DictDownUrl;
+                    ///下载新词库
+                    if (MessageBox.Show("有新词库,是否下载!\r\n", "有新词库", MessageBoxButtons.YesNo) == DialogResult.Yes)
                     {
-                        Thread.Sleep(2000);
-                        continue;
+                        Win.Login info = new Win.Login(false, Core.Win.WinInput.Input, 0);
+                        info.TopMost = true;
+                        info.Show();
+                      
+                        try
+                        {
+
+                            WebClient client = new WebClient();
+                            string filename = System.IO.Path.Combine(Win.WinInput.Input.AppPath, DateTime.Now.Ticks.ToString() + ".zip");
+                            client.DownloadFile(du, filename);
+                            Thread.Sleep(200);
+
+                            if (File.Exists(filename))
+                            {
+                                //下载完成解压
+                                updataok = ZipClass.UnZip(filename, System.IO.Path.Combine(Win.WinInput.Input.AppPath, "dict", InputMode.CDPath),"");
+                                Thread.Sleep(500);
+                                File.Delete(filename);//解压完成，删除下载的文件
+                                updataok = true;
+
+                                Win.WinInput.DictVersion = data.DictVersion;
+                                Program.MIme.SaveDictSetting();
+
+                                Program.MIme.InputIni();
+                                info.Close();
+                            }
+                        }
+                        catch
+                        {
+                            updataok = false;
+                        }
+                        
                     }
+
+                  
+                    #endregion
+                    return;
+                }
+
+            }
+            catch (Exception ex) { string ss = ex.ToString(); }
+        }
+        //private static void GetMyInfo()
+        //{
+        //    while (running)
+        //    {
+        //        Thread.Sleep(40000);
+        //        try
+        //        {
+        //            if (!Win.WinInput.Input.indexComplete)
+        //            {
+        //                Thread.Sleep(2000);
+        //                continue;
+        //            }
  
-                    qzxxiEntity.InfoEntity data = ApiClient.GetMyInfo(Win.WinInput.ApiUrl);
-                    if (!string.IsNullOrEmpty(data.InfoStr))
-                    {
-                        Program.InfoFrm.SendMsg(data);
-                    }
+        //            qzxxiEntity.InfoEntity data = ApiClient.GetMyInfo(Win.WinInput.ApiUrl);
+        //            if (!string.IsNullOrEmpty(data.InfoStr))
+        //            {
+        //                Program.InfoFrm.SendMsg(data);
+        //            }
                    
-                }
-                catch { Thread.Sleep(1000 * 60 * 20); }
+        //        }
+        //        catch { Thread.Sleep(1000 * 60 * 20); }
 
-                Thread.Sleep(1000 * 60 * 2);
-            }
-        }
+        //        Thread.Sleep(1000 * 60 * 2);
+        //    }
+        //}
 
 
-        private static void UpdateCloudDict()
-        {
-            while (running)
-            {
-                Thread.Sleep(20000);
-                try
-                {
-                    if (!Win.WinInput.Input.indexComplete)
-                    {
-                        Thread.Sleep(2000);
-                        continue;
-                    }
-                    if (!InputMode.OpenCould)
-                    {
-                        Thread.Sleep(1000 * 60 * 20);
-                        continue;
-                    }
-                    qzxxiEntity.ColdDictEntity data = ApiClient.GetCloudDictUpdate(Win.WinInput.ApiUrl);
-                    if (data.Value.Count > 0)
-                    {
-                        Win.WinInput.Input.ClouddDit.AddRange(data.Value);
-                        Program.MIme.SaveCloudDict();
-                        Thread.Sleep(1000 * 60 * 20);
-                    }
-                    if (!data.NextHave) Thread.Sleep(1000 * 60 * 60 * 8);
-                }
-                catch { Thread.Sleep(1000 * 60 * 20); }
-            }
-        }
+        //private static void UpdateCloudDict()
+        //{
+        //    while (running)
+        //    {
+        //        Thread.Sleep(20000);
+        //        try
+        //        {
+        //            if (!Win.WinInput.Input.indexComplete)
+        //            {
+        //                Thread.Sleep(2000);
+        //                continue;
+        //            }
+        //            if (!InputMode.OpenCould)
+        //            {
+        //                Thread.Sleep(1000 * 60 * 20);
+        //                continue;
+        //            }
+        //            qzxxiEntity.ColdDictEntity data = ApiClient.GetCloudDictUpdate(Win.WinInput.ApiUrl);
+        //            if (data.Value.Count > 0)
+        //            {
+        //                Win.WinInput.Input.ClouddDit.AddRange(data.Value);
+        //                Program.MIme.SaveCloudDict();
+        //                Thread.Sleep(1000 * 60 * 20);
+        //            }
+        //            if (!data.NextHave) Thread.Sleep(1000 * 60 * 60 * 8);
+        //        }
+        //        catch { Thread.Sleep(1000 * 60 * 20); }
+        //    }
+        //}
     }
 }
